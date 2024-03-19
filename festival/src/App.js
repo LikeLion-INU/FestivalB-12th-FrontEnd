1eabef8b48f4cdb57fc3e3bdde588ae1c7bf4eed
김멍몽 <wjdahrrla@gmail.com>
2024-03-19 12:23:11 +0900

    feat: mainPage 세팅

--- festival/src/App.js
+++ festival/src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/mainPage/main';
+import MyKeyWordPage from './pages/myKeyWordPage/myKeyWord';
+import IdealKeyWordPage from './pages/idealKeyWordPage/idealKeyWord';
+import MatchingPage from './pages/matchingPage/matching';
+import ResultPage from './pages/resultPage/result';
+import EndPage from './pages/endPage/end';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/'></Route>
-        <Route path='/myKeyWord'></Route>
-        <Route path='/idealKeyWord'></Route>
-        <Route path='/matching'></Route>
-        <Route path='/result'></Route>
-        <Route path='/end'></Route>
+        <Route path='/' element={<MainPage />}></Route>
+        <Route path='/myKeyWord' element={<MyKeyWordPage />}></Route>
+        <Route path='/idealKeyWord'element={<IdealKeyWordPage />}></Route>
+        <Route path='/matching' element={<MatchingPage />}></Route>
+        <Route path='/result' element={<ResultPage />}></Route>
+        <Route path='/end' element={<EndPage />}></Route>
       </Routes>
     </div>
   );
