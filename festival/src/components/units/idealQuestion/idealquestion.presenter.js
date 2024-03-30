d98579052b7e8eb3c2d90a04963d00f65e4d3f82
kwon <dohun_dev@naver.com>
2024-03-30 18:08:14 +0900

    이상형 키워드 페이지로 넘어왔을 때 띄울 modal 구현

--- festival/src/components/units/idealQuestion/idealquestion.presenter.js
+++ festival/src/components/units/idealQuestion/idealquestion.presenter.js
@@ -2,9 +2,40 @@ import * as S from "./idealquestion.styles";
 import { questions } from "./idealquestion.data";
 import AnimalComponent from "../animalUnit/animal.container";
 import { Progress } from "antd";
+import { Modal } from "antd";
+import { useState } from "react";
 export default function IdealPresenterPage(props) {
+  const [isModalOpen, setIsModalOpen] = useState(true);
   return (
     <>
+      {props.page_number == 0 ? (
+        <Modal
+          open={isModalOpen}
+          onOk={() => {
+            setIsModalOpen(false);
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <img src="/images/couple.png" />
+            <span>
+              <span style={{ color: "#ff7ca3", fontWeight: "700" }}>
+                이상형의 키워드
+              </span>
+              를 고르세요
+            </span>
+            <span>나의 키워드를 설정하고</span>
+            <span>이상형 키워드를 고를 수 있어요</span>
+          </div>
+        </Modal>
+      ) : (
+        ""
+      )}
       <S.Wrapper>
         <S.NaviGroup>
           <S.Navi src="/images/left_arrow.png" onClick={props.onClickPrev} />
